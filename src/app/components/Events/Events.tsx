a4efb0125c1f33b315a579afb03125791de50612
Biraj Dahal <dahalbiraj10@gmail.com>
2024-09-25 21:56:41 -0400

    updating events

--- src/app/components/Events/Events.tsx
+++ src/app/components/Events/Events.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import SectionHeader from '../SectionHeader/SectionHeader';
+import './styles.scss'
 
-const Navbar: React.FC = () => {
+const Events: React.FC = () => {
+  const calendarSrc: string = "https://calendar.google.com/calendar/embed?src=5efae451950b62f28ca23ee32edcbe893b63cdb7e9f07f89099cd179585b53fb%40group.calendar.google.com&ctz=America%2FNew_York&hl=en";
+
   return (
-    <div id="events" className="section"> 
-    <h1>Events Section</h1>
-
-  </div>
+    <div id="events" className="events">
+      <SectionHeader title='Events Calendar' />
+      <div className="calendar-container">
+        <iframe
+          src={calendarSrc}
+          title="Google Calendar"
+        ></iframe>
+      </div>
+    </div>
   );
 };
 
-export default Navbar;
+export default Events;
--- src/app/components/Resources/Resources.tsx
+++ src/app/components/Resources/Resources.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Navbar: React.FC = () => {
+const Resources: React.FC = () => {
   return (
-    <div id="resources" className="section"> 
+    <div id="resources" className="resources"> 
     <h1>Resources Section</h1>
   </div>
   );
 };
 
-export default Navbar;
+export default Resources;
